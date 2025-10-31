36edb54d4ca55f5ba2cedfc57ce82b409fed905b
SAO <saoelectronics@gmail.com>
2025-10-31 11:19:37 -0400

    Initial Publish

---
+++ background.js
@@ -0,0 +1,25 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "searchAllSources",
+    title: "Search All for '%s'",
+    contexts: ["selection"]
+  });
+});
+
+chrome.contextMenus.onClicked.addListener((info) => {
+  const query = info.selectionText;
+
+  const urls = [
+    `https://www.radwell.com/Search/?q=${encodeURIComponent(query)}`,
+    `https://www.classicautomation.com/catalogsearch/result/?q=${encodeURIComponent(query)}`,
+    `https://www.galco.com/catalogsearch/result/?q=${encodeURIComponent(query)}`,
+    `https://my.schneiderelectricrepair.com/PartSearch.aspx?Source=${encodeURIComponent(query)}`,
+    `https://www.mc-mc.com/Search?query=${encodeURIComponent(query)}&stockedItemsOnly=false&searchType=product`,
+    `https://www.google.com/search?q=${encodeURIComponent(query)}`
+  ];
+
+  chrome.windows.create({
+    url: urls,
+    focused: true
+  });
+});
