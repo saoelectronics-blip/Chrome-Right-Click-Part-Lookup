5b59f7143bf0e1e75857b9dcc07ef283543b5f1d
SAO <saoelectronics@gmail.com>
2025-12-18 12:32:26 -0500

    Added AFI Part Search

--- background.js
+++ background.js
@@ -1,21 +1,24 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.contextMenus.create({
-    id: "searchAllSources",
-    title: "Search All for '%s'",
-    contexts: ["selection"]
+  chrome.contextMenus.removeAll(() => {
+    chrome.contextMenus.create({
+      id: "searchAllSources",
+      title: "Search All for \"%s\"",
+      contexts: ["selection"]
+    });
   });
 });
 
 chrome.contextMenus.onClicked.addListener((info) => {
   const query = info.selectionText;
+  if (!query) return;
 
   const urls = [
     `https://www.radwell.com/Search/?q=${encodeURIComponent(query)}`,
     `https://www.classicautomation.com/catalogsearch/result/?q=${encodeURIComponent(query)}`,
     `https://www.galco.com/catalogsearch/result/?q=${encodeURIComponent(query)}`,
-    `https://my.schneiderelectricrepair.com/PartSearch.aspx?Source=${encodeURIComponent(query)}`,
     `https://www.mc-mc.com/Search?query=${encodeURIComponent(query)}&stockedItemsOnly=false&searchType=product`,
-    `https://www.google.com/search?q=${encodeURIComponent(query)}`
+    `https://www.google.com/search?q=${encodeURIComponent(query)}`,
+    `https://hub.afi-systems.com/product/product-list?part_num=${encodeURIComponent(query)}`
   ];
 
   chrome.windows.create({
